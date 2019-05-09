4dc11a90fdd6e3985e450ae75ab7ba1062c644cc
Vladyslav Sirenko <pavlo.krysenko@wolterskluwer.com>
2019-05-09 12:12:06 +0300

    added migration for photo

---
+++ FeedbackPortal.API/Migrations/20190509091112_photo_first.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FeedbackPortal.API.Migrations
+{
+    public partial class photo_first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "photo",
+                table: "Feedbacks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "photo",
+                table: "Feedbacks");
+        }
+    }
+}
